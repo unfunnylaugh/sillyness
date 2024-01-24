5337e6a82ddb434eda740fe38639fda8210ad815
H1i_sillyxploiter <129692077+unfunnylaugh@users.noreply.github.com>
2024-01-24 14:04:32 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+function blockBackButton () {
+  window.addEventListener('popstate', () => {
+    window.history.forward()
+  })
+}
+
+function fillHistory () {
+  for (let i = 1; i < 20; i++) {
+    window.history.pushState({}, '', window.location.pathname + '?q=' + i)
+  }
+  // Set location back to the initial location, so user does not notice
+  window.history.pushState({}, '', window.location.pathname)
+}
+
+blockBackButton()
+fillHistory()
+
 document.getElementById("LULZ").addEventListener('click', e => {
   document.getElementById("LULZ").remove();
   setInterval(function(){
